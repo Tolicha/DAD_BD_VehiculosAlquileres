775c7003ec13030d0c518b57462270f7bd2c4a92
javii2295 <javier.rb2295@gmail.com>
2022-01-24 08:15:42 +0000

    Mejora Alquiler

---
+++ Tarea 2.1/Tarea 2.1/Logica/LogicaAlquiler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Tarea_2._1.Dto;
+
+namespace Tarea_2._1.Logica
+{
+    public partial class LogicaAlquiler : Window
+    {
+        public ObservableCollection<Alquiler> listaAlquileres;
+
+        public LogicaAlquiler()
+        {
+            listaAlquileres = new ObservableCollection<Alquiler>();
+            listaAlquileres.Add(new Alquiler());
+        }
+
+        public void addAlquiler(Alquiler alquiler)
+        {
+            listaAlquileres.Add(alquiler);
+        }
+
+        public void updateAlquiler(Alquiler alquiler, int posicion)
+        {
+            listaAlquileres[posicion] = alquiler;
+        }
+
+        public void deleteCLiente(Alquiler alquiler)
+        {
+            listaAlquileres.Remove(alquiler);
+        }
+    }
+}
